0f39d458b737e89ddd18ceec8f2e04cacae0f029
henghengyh <yihenglee6@gmail.com>
2026-03-11 16:12:08 +0800

    Update testsuite for AddressBookParserTest.java parseCommand_list

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -85,7 +85,11 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_list() throws Exception {
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
-        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+        assertThrows(ParseException.class,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE),
+                () -> parser.parseCommand(ListCommand.COMMAND_WORD + " 3")
+        );
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " sort") instanceof ListCommand);
     }
 
     @Test
